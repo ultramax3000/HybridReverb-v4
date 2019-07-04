2f0a2b712fcd3155838074390b26f76c2d391fcc
ultramax3000 <32769400+ultramax3000@users.noreply.github.com>
2019-07-04 16:39:54 +0200

    Add files via upload
    Added mire IR paths and delay-times.

--- HRv4_master.m
+++ HRv4_master.m
@@ -4,9 +4,13 @@ clear all; clc; close all
 
 %Specify files for IR and Dry Signal
 audioInput = 'vox10.wav';
-IRinputMono = 'academy_yard_mono.wav';       %IR Mono
-%IRinputMono = 'nave_cathedral_mono.wav';    %IR Mono
-%IRinputStereo = 'nave_cathedral.wav';       %IR Stereo
+
+%IRinputMono = 'academy_yard_mono.wav';     %IR Mono
+IRinputMono = 'nave_cathedral_mono.wav';   %IR Mono
+%IRinputMono = 'phipps_hall_huddersfield_mono.wav'; % EDR doesn't work (?)
+%IRinputMono = 'BM7_Medium_Chamber_mono.wav';
+
+%IRinputStereo = 'nave_cathedral.wav';      %IR Stereo
 
 %Read audio-files into arrays
 [IR, fs,] = audioread(IRinputMono);
@@ -25,11 +29,11 @@ R = 3.6;                %relates to the Q-Factor
 Q = sqrt(R) / (R-1);
 
 % FDN Delay Times
-%delayTimes = [89 97 107 113 149 211 263 293 401 421 433 443 577 601 641 661]';
-%delayTimes = [193 373 421 499 569 617 677 751 823 907 929 947 971 991 1019 1039]';
-delayTimes = [443 1949 4409 5417 6421 7537 8863 9049 10799 11177 12791 13679 14891 15287 16339 17657]';
+%delayTimes = [89 97 107 113 149 211 263 293 401 421 433 443 577 601 641 661];
+%delayTimes = [193 373 421 499 569 617 677 751 823 907 929 947 971 991 1019 1039];
+%delayTimes = [443 1949 4409 5417 6421 7537 8863 9049 10799 11177 12791 13679 14891 15287 16339 17657];
 
-%delayTimes = prime_power_delays(fs,16,5,100);
+delayTimes = prime_power_delays(fs,16,1,100);
 
 %% Early Reflections Processing
 %Truncate the IR to only the Early Reflections, using a 20ms Window:
@@ -57,7 +61,7 @@ numControlFreqs = 100;
 
 %% Optimization
 %Optimize parametric filter gains from magnitude response (Linear Solution)
-[gainsLin]   = optFDNfiltLin(T60full,delayTimes,centerFreqs, shelvingFreqs,R,fs,numControlFreqs);
+[gainsLin]   = optFDNfiltLin(T60full,delayTimes',centerFreqs, shelvingFreqs,R,fs,numControlFreqs);
 
 %Optimize parametric filter gains from T60 times (Nonlinear Solution)
 %[gains]   = optFDNfilt(T60full,delayTimes, centerFreqs, shelvingFreqs,R,fs,numControlFreqs);
@@ -67,7 +71,7 @@ numControlFreqs = 100;
 
 %Excite FDN with an Impulse:
 x = [0; 1; 0];
-FDNimp = FDN16(x,fs,centerFreqs,shelvingFreqs,R,gainsLin,delayTimes);
+FDNimp = FDN16(x,fs,centerFreqs,shelvingFreqs,R,gainsLin,delayTimes');
 
 %16x16 FDN:
 %FDNout = FDN16(audio,fs,centerFreqs,shelvingFreqs,R,gainsLin,delayTimes);
