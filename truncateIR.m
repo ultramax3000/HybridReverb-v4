e5077bd810d938c194890ceabb70f993eced6924
mas <mas@client3268>
2019-07-05 16:37:35 +0200

    fixed it

--- truncateIR.m
+++ truncateIR.m
@@ -1,69 +1,56 @@
-function [truncTime, truncIR] = truncateIR(IR, fs, winSize)
+function [truncTime, truncIR] = truncateIR(IR, Fs, winSize)
 %__________________________________________________________________________
-%[truncation time, truncated array] =  truncate(array with IR, samplerate, window size in ms)
-
+%[truncation time, truncated array] =
+% truncate(array containing IR, sampling frequency, window size in ms)
+%%
 %Determines the end of the early reflections and truncates
-%the IR. Output time of truncation in msec and truncated
+%the IR. The output time of truncation in msec and truncated
 %array are output.
+%
+%Rebecca Stewart
+%July 2006
 %__________________________________________________________________________
-
-%Function requires at least 3 input arguments
+%Function requires at least 3 inputs argument
 if nargin < 3
     error('Not enough input arguments.');
 end
-truncTime = []; 
-truncIR = [];
-
-%Window incoming IR file
-window = fs * winSize/1000;
-sizeIR = length(IR);
-
-iterations = floor(sizeIR(1)/window);
-time = linspace(0, sizeIR(1)/fs, iterations); %time increments
+truncTime = []; truncIR = [];
+%Window wav
+window = Fs * winSize/1000;
+sizeInput = length(IR);
+iterations = floor(sizeInput(1)/window);
+time = linspace(0,sizeInput(1)/Fs,iterations); %time increments
 
 for i=1:iterations
-    winIR = IR(window * (i-1)+ 1:window * i);
-    
-    %Find STANDARD DEVIATION and MEAN of window
-    sigma = std(winIR);
-    mu = mean(winIR);
-    
+    winIn = IR(window * (i-1)+1:window*i);
+    %Find standard deviation and mean of window
+    sigma = std(winIn);
+    mu = mean(winIn);
     %Calculate percentage of samples outside 1 standard deviation
-    outlier = 0;
-    
+    out1 = 0;
     for n=1:window
-        if winIR(n) < (mu - sigma) | winIR(n) > (mu + sigma)
-            outlier = outlier + 1;
+        if winIn(n) < (mu - sigma) | winIn(n) > (mu + sigma);
+            out1 = out1 + 1;
         end
     end
-    
     %Store in array
-    percent1(i) = outlier/window;
+    percent1(i) = out1/window;
 end
-
-%Check for the first threshold crossing over 30% during the first 40 to
-%60ms
-met30 = [];
+%Check for first time over 30% in first 40 to 160 ms
 for i=8:-1:3
     if percent1(i) >= .3
         met30 = i;
     end
 end
-
-%Find local maximum beyond 60ms after first crossing of threshold
-[~, truncIndex] = max(percent1(met30:met30+1));
-
-truncTime = 1000*time(truncIndex + met30-1);
-tempIR = IR(1:ceil(truncTime/1000 * fs)+32);
-
-%Window the end of the IR with the second half of a hann window 
+%Find local max over the 60 ms after the first crossing of the threshold
+[maxValue, truncIndex] = max(percent1(met30:met30+1));
+truncTime = 1000*time(truncIndex+met30-1);
+tempIR = IR(1:ceil(truncTime/1000 * Fs)+32);
+%Hann window
 w = hann(64);
-ERlength = length(tempIR);
-tempIR((ERlength-31):ERlength) = (w(33:64))' .* tempIR((ERlength-31):ERlength);
-%tempIR((ERlength-31):ERlength) = w(33:64) .* tempIR((ERlength-31):ERlength);
-
+%Window the end of the IR with the falling edge of window function
+l = length(tempIR);
+tempIR((l-31):l) = w(33:64) .* tempIR((l-31):l);
 %Return truncated IR
 truncIR = tempIR;
-
 end
-
