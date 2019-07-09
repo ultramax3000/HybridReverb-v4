19399e93db9738a875b25e1785736092d35afcf2
mas <mas@client3268.office.ableton.com>
2019-07-09 20:39:21 +0200

    x0 is now calculated as the mean of tau.
    Also prototype gain is 1 dB.

--- optFDNfiltNonlin.m
+++ optFDNfiltNonlin.m
@@ -1,56 +1,65 @@
-function [gainsNonlin] = optFDNfiltNonlin(T60full,delayTimes,centerFreqs,shelvingFreqs,...
-    R,fs,numControlFreqs)
+function [gainsNonlin] = optFDNfiltNonlin(T60bands,delayTimes,centerFreqs,shelvingFreqs,...
+    R,fs,numControlFreqs,initGain)
 fftLen = 2^16;
 
 numFreq = length(centerFreqs) + length(shelvingFreqs);
 shelvingOmega = hertz2rad(shelvingFreqs, fs);
 centerOmega = hertz2rad(centerFreqs, fs);
+
  
+% Convert Frequency dependant T60 times to attenuation per sample (Schlecht
+% EQ (2)-(5) & (27).
+
+for i = 1:length(delayTimes)
+    targetGains(:,i) = -60*(1./(fs*T60bands))*delayTimes(i,:);
+end
+
 % control frequencies are spaced logarithmically
 controlFrequencies = logspace(log10(1), log10(fs/2.1),numControlFreqs+1);
+
+% Interpolate for target magnitude response via attenuation gain per sample
+ targetFreqs = [1, centerFreqs fs];
  
-% target magnitude response via command gains
-targetFreqs = [1, centerFreqs fs];
-targetGains = [1; -1; 1; -1; 1; -1; 1; -1; 1; 1]*10; % dB
-targetInterp = interp1(targetFreqs, targetGains, controlFrequencies)';
+ for j=1:size(targetGains,2)
+ tau(:,j) = interp1(targetFreqs, targetGains(:,j), controlFrequencies)';
+ end
+
  
-%% desgin prototype of the biquad sections
+%% Desgin prototype of the filter magnitude responses
 prototypeGain = 1; % dB
 prototypeGainArray = prototypeGain * ones(numFreq+1,1);
 prototypeSOS = proportionalParametricEQ(centerOmega, shelvingOmega, R, prototypeGainArray);
 [B,prototypeH,prototypeW] = probeSOS (prototypeSOS, controlFrequencies, fftLen, fs);
-% B = B / prototypeGain; % dB vs control frequencies ("Interaction Matrix")
- 
+B= B/prototypeGain;
+
 % Plot the Magnitude response of the Filters
-figure; 
-semilogx(prototypeW,mag2db(abs(prototypeH)))
-ylim([0 11])
-xlim([10 fs/2])
-grid on;
-title('Prototype Magnitude Response (Interaction Matrix)')
-xlabel('Frequency [Hz]')
-ylabel('Magnitude [dB]')
+% figure; 
+% semilogx(prototypeW,mag2db(abs(prototypeH)))
+% ylim([0 11])
+% xlim([10 fs/2])
+% grid on;
+% title('Prototype Magnitude Response (Interaction Matrix)')
+% xlabel('Frequency [Hz]')
+% ylabel('Magnitude [dB]')
  
 %% Compute optimal parametric EQ gains (nonlinear solution)
-x0 = ones(numFreq+1, 1); % initial gain in dB
-upBound = 10*ones(numFreq+1, 1); % bounds for the gains
+
+% Initial gain in dB
+%x0 = initGain*ones(numFreq+1, 1); 
+
+% Bounds for the gains (between -10 and 10 dB)
+upBound = 100*ones(numFreq+1, 1); 
 lowBound = -upBound;
 
 %optGainNonlin = lsqnonlin(nonlinFun,x0,upBound,lowBound);
 options = optimoptions('fmincon','SpecifyObjectiveGradient',true); % indicate gradient is provided 
 
-% Convert Frequency dependant T60 times to attenuation per sample (Schlecht
-% EQ (2)-(5) & (27), taking the average of all delay-line times (experimental solution.
-
-%Calculate tau (27) for each dealy-line: 
-tau = zeros(length(T60full),length(delayTimes)); 
-for i = 1:length(delayTimes)
-    tau(:,i) = -60*(1./(fs*T60full))*delayTimes(i,:);
-end
-
-%Optimize gains for each delay.line
+%Optimize gains for each delay-line
 optGainNonlin = zeros(11,length(delayTimes));
 for k = 1:size(tau,2)
+    % Calculate x0 as the arithmetic mean of tau (32)
+    x0 = zeros(numFreq+1, 1);
+    x0(1) = mean(tau(:,k));
 optGainNonlin(:,k) = fmincon(@(gamma) nonlinFun(gamma, B, tau(:,k)),x0,[],[],[],[],lowBound, upBound,[], options);
 end
 
@@ -75,7 +84,7 @@ gainsNonlin = optGainNonlin;
 % legend('Target', 'Actual EQ', 'Design EQ','Location','SouthEast');
  
 figure
-heatmap(B);
+imagesc(B);
 title(['Interaction Matrix with Filter Magnitude Responses (' num2str(numControlFreqs) ' Control Frequencies)']) 
 
 end
