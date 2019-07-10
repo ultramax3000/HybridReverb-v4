bb48dd8d423889eaa515fe44c9a3a7ea0fe1aa8b
mas <mas@client3268.office.ableton.com>
2019-07-10 19:37:41 +0200

    Added silence to input signal (otherwise FDN processing is interrupted).
    Moved plots from FDN function into this script.

--- HRv4_stereo.m
+++ HRv4_stereo.m
@@ -14,32 +14,38 @@ clear all; clc; close all
 %Here, both a mono- and stereo IR have to be specified,because analysis and 
 %optimization only make sense to be performed on a mono IR
 
-%IR Mono:
-%IRinputMono = 'academy_yard_mono.wav';     
-IRMono = 'nave_cathedral_mono.wav';   
-%IRinputMono = 'phipps_hall_huddersfield_mono.wav'; % EDR doesn't work (?)
-%IRinputMono = 'BM7_Medium_Chamber_mono.wav';
+%IR Mono 
+%IRMono  = 'TerrysWarehouse_mono.wav';
+%IRMono  = 'nave_cathedral_mono.wav';   
+%IRMono = 'BM7_Medium_Chamber_mono.wav';
+IRMono = 'BM7_Small_Room_mono.wav';
 
-IRStereo = 'nave_cathedral.wav';      %IR Stereo
+%IR Stereo
+%IRStereo   = 'TerrysWarehouse.wav';
+%IRStereo = 'nave_cathedral.wav';     
+%IRStereo = 'BM7_Medium_Chamber.wav';
+IRStereo = 'BM7_Small_Room.wav';
 
 %Read audio-files into arrays
 [IR, fs,] = audioread(IRMono);
 [IRstereo, fs2] = audioread(IRStereo);
 
-%Make truncated chunk of IR to use as "pre-filtered impulse"
-frameSize = 15;
+% %Make truncated chunk of IR to use as "pre-filtered impulse"
+frameSize = 5;
 minFrameLen = round(fs*frameSize/1000);
 truncWin = hann(2*minFrameLen); %Use second half of a hann window for smoothing.
-truncWin = truncWin((minFrameLen+1):end);
-
+truncWin = truncWin((minFrameLen+1):end); 
 audio = IR(1:minFrameLen,1).*truncWin;
 
-%Make truncated chunk of IR to use as "pre-filtered impulse"
-% frameSize = 10;
+% %Make truncated chunk of IR to use as "pre-filtered impulse"
+% frameSize = ;
 % minFrameLen = round(fs*frameSize/1000);
 % truncWin = hann(minFrameLen); %Use second half of a hann window for smoothing.
-% 
 % audio = IR(1:minFrameLen,1).*truncWin;
+
+%Ad some seconds of silence after signal:
+silence = 7; %in seconds
+audio = [audio; zeros(round((length(IR)/2))-length(audio),1)];
 
 %Make sure that sampling frequencies are equal for IR and dry signal
 if (fs > fs2 || fs < fs2)
@@ -47,8 +53,8 @@ if (fs > fs2 || fs < fs2)
 end
 
 %% Global Parameters
-% Lowest and highes freqs have been adjusted to be equally spaced in
-% interaction Matrix
+%Shelving freqs have been adjusted to be equally spaced in
+% interaction Matrix (Schlecht's suggestion)
 centerFreqs = [62.5, 125, 250, 500, 1000, 2000, 4000, 8000];
 shelvingFreqs = [43, 11360];
 R = 3.6;                %relates to the Q-Factor
@@ -64,6 +70,9 @@ Q = sqrt(R) / (R-1);
      31.469299 38.294399 46.6838 55.456699 65.175499 76.824303 88.562302 101.278... 
      115.397003 130.501999].*(fs/1000));
 
+ %Steward Delay Times
+% delayTimes = [4999 4639 4243 3797 3547 3191 2789 2411 2137 1931 1901 1847 1409 853 601 457];
+% 
 % FDN Delay Times 8x8
 %delayTimes = round([1.39 2.51 11.1 19.4 41.2 60.2 73.6 96.6].*(fs/1000));
 %delayTimes = prime_power_delays(fs,8,1,200);
@@ -71,7 +80,7 @@ Q = sqrt(R) / (R-1);
 %% Early Reflections Processing
 %Truncate the IR to only the Early Reflections, using a 20ms Window:
 [truncTime, truncIRL] = truncateIR(IRstereo(:,1), fs, 20);
-[~, truncIRR] = truncateIR(IRstereo(:,2), fs, 20);
+ truncIRR = IRstereo(1:length(truncIRL),2);
 
 %CONVOLVE the IR with the dry audio:
 dryWet = 100;
@@ -92,7 +101,7 @@ numControlFreqs = 100;
 %[T60bands100] = calcEDR(IR,fs,frameSize,overlap,windowType,numControlFreqs);
 
 %Calculate global T60 acording to ISO 3382-1:2009:
-[T60]=iosr.acoustics.irStats(IRMono,'graph',true,'spec','mean');
+%[T60]=iosr.acoustics.irStats(IRMono,'graph',true,'spec','mean');
 
 %% Optimization
 %Optimize parametric filter gains from magnitude response (Linear Solution)
@@ -140,6 +149,24 @@ wet(:,2) = (convER(:,2) + FDNstereo(:,2));
 processedAudio(:,1) = (mix/100)*(wet(:,1)) + ((100-mix)/100)*audio;
 processedAudio(:,2) = (mix/100)*(wet(:,2)) + ((100-mix)/100)*audio;
 
+%Rescale
+%processedAudio = processedAudio*1000;
+
+%% Plot
+dt = 1/fs;
+tinput = 0:dt:(length(audio)*dt)-dt;
+plot(tinput,processedAudio(:,1),'g'); hold on;
+plot(tinput,audio,'k'); xlabel('Seconds'); ylabel('Amplitude');
+title('16x16 Feedback Delay Network');
+
+% Spectrogram
+figure(1)
+subplot(2,1,1);
+specgram(audio);
+title('Dry Signal');
+subplot(2,1,2);
+specgram(processedAudio(:,1));
+title('Wet Signal');
 %% Demo against Convolution only.
 % 
 % %Generate only convolved signal
